9fa70973b29e2629f4cc03eaa0070506e25587a1
Just-Artur0 <okodobre@gmail.com>
2024-07-15 17:40:12 +0100

    Add files via upload

---
+++ console.js
@@ -0,0 +1,11 @@
+console.log('Hiiiii, Why are you inspecting?'); String
+
+function button(){
+    //alert('sup')
+    x = document.getElementById("button")
+    //x.innerHTML = "hi"
+}
+var inputBox = document.getElementById("myInput");
+
+var userInput = inputBox.value;
+  
